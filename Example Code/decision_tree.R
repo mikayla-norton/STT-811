46fb01f080194d26f7d78b849b7d7ceb01194368
Mikayla Norton <norton18@msu.edu>
2023-04-05 16:46:12 -0400

    push end of march changes

---
+++ Example Code/decision_tree.R
@@ -0,0 +1,18 @@
+library(tree)
+library(ISLR2)
+
+OJ <- ISLR2::OJ
+split_pct <- 0.7
+n <- length(OJ$Purchase)*split_pct # train size
+row_samp <- sample(1:length(OJ$Purchase), n, replace = FALSE)
+train <- OJ[row_samp,]
+test <- OJ[-row_samp,]
+
+OJ_tree <- tree(Purchase ~ PriceDiff + LoyalCH, data = train)
+
+plot(OJ_tree)
+text(OJ_tree)
+tree_pred <- predict(OJ_tree, test, type = 'class')
+
+confusionMatrix(tree_pred, test$Purchase)
+
